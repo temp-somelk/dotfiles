df3f772c038317bdfd2f91ad2c702ff7c1dee015
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-10-31 20:25:05 +0530

    replace shutdown with poweroff -i

--- .config/waybar/power.sh
+++ .config/waybar/power.sh
@@ -1,1 +1,1 @@
-swaynag -t warning -m "Power Menu Options" -Z "Lock" "swaylock" -Z "Logout" "swaymsg exit" -Z "Suspend" "systemctl suspend" -Z "Reboot" "systemctl reboot" -Z "Shutdown" "systemctl shutdown -i"
+swaynag -t warning -m "Power Menu Options" -Z "Lock" "swaylock" -Z "Logout" "swaymsg exit" -Z "Suspend" "systemctl suspend" -Z "Reboot" "systemctl reboot" -Z "Shutdown" "systemctl poweroff -i"
