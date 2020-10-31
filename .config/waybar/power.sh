5b3a8dd948c5f1988df908450e3126aeabac8d5a
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-10-31 19:37:25 +0530

    added power module

---
+++ .config/waybar/power.sh
@@ -0,0 +1,1 @@
+swaynag -t warning -m 'Power Menu Options' -b "Lock" "swaylock" -b "Logout" "swaymsg exit" -b "Suspend" "systemctl suspend" -b "Reboot" "systemctl reboot" -b "Shutdown" "systemctl shutdown -i"
