bb73c0f41ed03058ef15572f5d4e8d974296d575
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-11-05 17:49:04 +0530

    changed order

--- .config/waybar/power.sh
+++ .config/waybar/power.sh
@@ -1,1 +1,1 @@
-swaynag -t message -m "Power Menu Options" -Z "Lock" "swaylock" -Z "Logout" "swaymsg exit" -Z "Suspend" "systemctl suspend" -Z "Reboot" "systemctl reboot" -Z "Shutdown" "systemctl poweroff -i"
+swaynag -t message -m "Power Menu Options" -Z "Lock" "swaylock" -Z "Suspend" "systemctl suspend" -Z "Logout" "swaymsg exit" -Z "Reboot" "systemctl reboot" -Z "Shutdown" "systemctl poweroff -i"
