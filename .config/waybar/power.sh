2853d288569b6192ec35ecdb61c003955ccb3dcb
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-11-07 02:20:24 +0530

    ignore inhibitors for power options

--- .config/waybar/power.sh
+++ .config/waybar/power.sh
@@ -1,1 +1,1 @@
-swaynag -t message -m "Power Menu Options" -Z "Lock" "swaylock" -Z "Suspend" "systemctl suspend" -Z "Logout" "swaymsg exit" -Z "Reboot" "systemctl reboot" -Z "Shutdown" "systemctl poweroff -i"
+swaynag -t message -m "Power Menu Options" -Z "Lock" "swaylock" -Z "Suspend" "systemctl suspend -i" -Z "Logout" "swaymsg exit" -Z "Reboot" "systemctl reboot -i" -Z "Shutdown" "systemctl poweroff -i"
