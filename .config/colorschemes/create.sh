83ddd83e9770a4e4387efc87130fcd1cc6360f02
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-12-01 01:08:29 +0530

    included waybar and wofi

--- .config/colorschemes/create.sh
+++ .config/colorschemes/create.sh
@@ -4,10 +4,21 @@ num_elements=$(($(echo "$data" | jq length)-1))
 
 for i in mako sway swaynag swaylock termite
 do
+	cp ../$i/config.main ../$i/config.$filename
 	for j in $(seq 1 $num_elements)
 	do
 		key=\$$(echo $data | jq -r "keys_unsorted[$j]")
 		val=$(echo $data | jq -r ".[keys_unsorted[$j]]")
-		sed "s/$key/$val/g" ../$i/config.main
+		sed -i "s/$key/$val/g" ../$i/config.$filename
+	done
+done
+for i in waybar wofi
+do
+	cp ../$i/style.css.main ../$i/style.css.$filename
+	for j in $(seq 1 $num_elements)
+	do
+		key=\$$(echo $data | jq -r "keys_unsorted[$j]")
+		val=$(echo $data | jq -r ".[keys_unsorted[$j]]")
+		sed -i "s/$key/$val/g" ../$i/style.css.$filename
 	done
 done
