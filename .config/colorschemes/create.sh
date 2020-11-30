dead17fc4e753723ab2890732f2e9855803880c7
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-11-30 21:12:23 +0530

    colorscheme script

--- .config/colorschemes/create.sh
+++ .config/colorschemes/create.sh
@@ -1,3 +1,9 @@
-eval $(cat variables.json | jq -r 'keys[] as $k | "\($k)=\(.[$k])"')
+variables=$(cat variables.json)
+num_elements=$(($(cat variables.json | jq length) - 1))
 
+echo $num_elements
 
+for i in $(seq 0 $num_elements)
+do
+	echo "$i"
+done
--- .config/colorschemes/fast_but_messy.sh
+++ .config/colorschemes/fast_but_messy.sh
@@ -1,8 +1,12 @@
 eval $(cat variables.json | jq -r 'keys[] as $k | "\($k)=\(.[$k])"')
 
-for i in mako swaylock swaynag termite
+for i in sway mako swaylock swaynag termite
 do
 	sed "s/\$accent/$accent/g;s/\$text/$text/g;s/\$texlight/$texlight/g;s/\$background/$background/g;s/\$black0/$black8/g;s/\$red/$red/g;s/\$green/$green/g;s/\$yellow/$yellow/g;s/\$blue/$blue/g;s/\$magenta/$magenta/g;s/\$cyan/$cyan/g;s/\$white/$white/g;s/\$gray/$gray/g;s/\$grey/$grey/g" ../$i/config.main > ../$i/config.$name
+done
+
+for i in mako swaylock swaynag termite
+do
 	ln -sf ../$i/config.$name ../$i/config
 done
 
