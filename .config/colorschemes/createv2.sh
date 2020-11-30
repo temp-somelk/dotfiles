c9d15dc230b6f3d1332cb5a11f7e7d73bf5fdef6
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-12-01 02:33:22 +0530

    version 2, a lot faster, smaller and neater

--- .config/colorschemes/createv2.sh
+++ .config/colorschemes/createv2.sh
@@ -1,11 +1,15 @@
 name=$(cat colors.json | jq -r '.name')
 data=$(cat colors.json | jq -jr 'keys[] as $k | "s/\\$\($k)/\(.[$k])/g; "')
 
-for i in sway mako swaylock swaynag termite
+for i in mako swaylock swaynag termite
 do
 	sed "$data" ../$i/config.main > ../$i/config.$name
+	ln -sf ../$i/config.$name ../$i/config
 done
 for i in waybar wofi
 do
 	sed "$data" ../$i/style.css.main > ../$i/style.css.$name
+	ln -sf ../$i/style.css.$name ../$i/style.css
 done
+sed "$data" ../sway/config.main > ../sway/config.$name
+sed -i "s/include\ config.*/include\ config.$name/" ../sway/config
