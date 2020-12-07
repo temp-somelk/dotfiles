e031d385091c9ea4d3863c7466ffffe348f7f68e
Anchit Roy <26524638+anchit-roy@users.noreply.github.com>
2020-12-07 23:36:19 +0530

    updated playerctl

--- .config/waybar/playerctl.sh
+++ .config/waybar/playerctl.sh
@@ -1,1 +1,1 @@
-echo '{"text": "'$(playerctl metadata --format "{{ artist }} - {{ title }}")'", "alt": "'$(playerctl status)'", "class": "'$(playerctl status)'"}'
+echo '{"text": "'$(playerctl metadata --format "{{ title }} - {{ artist }}")'", "alt": "'$(playerctl status)'", "class": "'$(playerctl status)'"}'
