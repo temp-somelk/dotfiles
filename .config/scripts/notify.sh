bc96187fcf1f356312f061539909a06360045448
somelazykoala <26524638+somelazykoala@users.noreply.github.com>
2022-01-27 06:31:42 +1300

    delete notify.sh

--- .config/scripts/notify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-app="$(playerctl metadata --format {{playerName}})"
-artist="$(playerctl metadata --format {{artist}})"
-album="$(playerctl metadata --format {{album}})"
-title="$(playerctl metadata --format {{title}})"
-
-notify-send --urgency=normal --expire-time=5000 --app-name="$app" "$title" "$artist\n$album"
