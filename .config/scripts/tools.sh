8950441b9eacfd2de99619d01ef7736606411fb0
artfrowl <26524638+artfrowl@users.noreply.github.com>
2022-10-06 11:33:43 +0530

    removed dictionary, added ocr

--- .config/scripts/tools.sh
+++ .config/scripts/tools.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-TOOLS=('matrix codes' 'color picker' 'dictionary' 'emoji picker' 'calculator' 'copy to file' 'password manager')
-TOOL="$(( IFS=$'\n'; echo "${TOOLS[*]}" ) | dmenu-wl -p "tools:" -i -h 30 -b -r -fn JetBrainsMono -nb "#00010a" -nf "#e5e9f0" -sf "#e5e9f0" -sb "#3a575c" | tr -d '[:blank:]' | tr '[:upper:]' '[:lower:]')"
+MENU="tofi"
+TOOLS=('matrix codes' 'color picker' 'ocr' 'emoji picker' 'calculator' 'copy to ~/tmp' 'password manager')
+TOOL="$(( IFS=$'\n'; echo "${TOOLS[*]}" ) | $MENU --prompt-text "tools:" | tr -d '[:blank:]' | tr '[:upper:]' '[:lower:]')"
 exec "$HOME/.config/scripts/tools.$TOOL.sh"
 
 # maybe just use conditional statements
